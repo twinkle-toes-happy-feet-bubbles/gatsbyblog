a0c27ab92e41bc1cfccea13d71ed5ec942e72ef5
twinkle-toes-happy-feet-bubbles <72191306+twinkle-toes-happy-feet-bubbles@users.noreply.github.com>
2022-04-30 18:33:49 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
+        <p>&copy; {new Date().getFullYear()} Cryptofrog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://twitter.com/notprajyoth?t=89-7ZtmBooC5518G3Ys3cw&s=09">Mopare</a></p>
       </footer>
     </div>
   )
