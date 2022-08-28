100c354b0c7783657db7e780da57686bc97f99d8
twinkle-toes-happy-feet-bubbles <72191306+twinkle-toes-happy-feet-bubbles@users.noreply.github.com>
2022-08-29 00:33:10 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `CryptoFrog - The tech paradise `,
+        short_name: `CryptoFrog`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
